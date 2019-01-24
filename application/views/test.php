2e274b06a75d21e94812dd422e9445748b8b0f18
lakshang <lakshan.1998@gmail.com>
2019-01-24 22:09:29 +0530

    Modified test view

--- application/views/test.php
+++ application/views/test.php
@@ -7,12 +7,13 @@
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <meta charset="UTF-8">
         <title>Wish List</title>
+
     </head>
     <body>
         <br>
         <button class="btn btn-danger logout-user">Logout</button>
         <div class="container">
-
+            <h3><?php echo $_COOKIE["username"]; ?>'s Wishlist</h3>
             <table class="table">
                 <thead>
                     <tr>
@@ -20,8 +21,8 @@
                         <th>URL</th>
                         <th>Price</th>
                         <th>Priority</th>
-                        <th>Action</th>
-                    </tr>
+                        <th>Owner-Id</th>
+
                     <tr>
                         <td><input class="form-control title-input"></td>
                         <td><input class="form-control url-input"></td>
@@ -32,8 +33,9 @@
                                 <option value="2">2 - Medium</option>
                                 <option value="3">3 -Low</option>
                             </select></td>
-                        <td><input class="form-control user-id" value="<?php echo $_COOKIE['user_id']; ?> "></td>
+                        <td><input class="form-control user-id" value="<?php echo $_COOKIE['user_id']; ?> " disabled=""></td>
                         <td><button class="btn btn-primary add-item">Add</button></td>
+                        <td><button class="btn btn-info share-list">Share</button></td>
                     </tr>
                 </thead>
                 <tbody class="items-list"></tbody>
@@ -42,7 +44,7 @@
 
         <script type="text/template" class="items-list-template">
             <td><span class="title"><%= title %></span></td>
-            <td><span class="url"><%= url %></span></td>
+            <td><span class="url"><a href="<%= url %>"><%= url %></a></span></td>
             <td><span class="price"><%= price %></span></td>
             <td><span class="priority"><%= priority %></span></td>
             <td><button class="btn btn-warning edit-item">Edit</button>
@@ -97,21 +99,27 @@
                     var url = this.$('.url').html();
                     var price = this.$('.price').html();
                     var priority = this.$('.priority').html();
-                    this.$('.title').html('<input type="text" class="form-control title-update" value="' + title + '" disabled>');
-                    this.$('.url').html('<input type="text" class="form-control url-update" value="' + url + '" disabled>');
-                    this.$('.price').html('<input type="text" class="form-control price-update" value="' + price + '" disabled>');
+                    this.$('.title').html(title);
+                    this.$('.url').html(url);
+                    this.$('.price').html(price);
                     this.$('.priority').html('<input type="text" class="form-control priority-update" value="' + priority + '">');
                 },
                 update: function () {
                     this.model.set('priority', $('.priority-update').val());
-                    this.model.save(null, {
-                        success: function (response) {
-                            console.log('Successfully UPDATED item with id:' + response.toJSON().item_id);
-                        },
-                        error: function () {
-                            console.log('Failed to UPDATED Item');
-                        }
-                    });
+                    if ($('.priority-update').val() === "1" || $('.priority-update').val() === "2" || $('.priority-update').val() === "3") {
+                        this.model.save(null, {
+                            success: function (response) {
+                                console.log('Successfully UPDATED item with id:' + response.toJSON().item_id);
+                                alert('Update Successful');
+                            },
+                            error: function () {
+                                console.log('Failed to UPDATED Item');
+                            }
+                        });
+                    } else {
+                        alert('Please select a proper priority (1-High/2-Medium/3-Low)');
+                    }
+
                 },
                 cancel: function () {
                     itemsView.render();
@@ -120,6 +128,7 @@
                     this.model.destroy({
                         success: function (response) {
                             console.log('Successfully DELTED item with id:');
+                            alert('Item Delete Successful');
                         },
                         error: function () {
                             console.log('Failed to DELETE Item');
@@ -169,29 +178,33 @@
             var itemsView = new ItemsView();
             $(document).ready(function () {
                 $('.add-item').on('click', function () {
-                    var item = new Item({
-                        title: $('.title-input').val(),
-                        url: $('.url-input').val(),
-                        price: $('.price-input').val(),
-                        priority: $('.priority-input').val(),
-                        user_id: $('.user-id').val()
-                    });
-                    $('.title-input').val('');
-                    $('.url-input').val('');
-                    $('.price-input').val('');
-                    $('.priority-input').val('');
-                    items.add(item);
-                    item.save({
-                        success: function (response) {
-                            _.each(response.toJSON(), function (item) {
-                                console.log('Successfully Added an Item');
+                    if ($('.title-input').val() === "" || $('.url-input').val() === "" || $('.price-input').val() === "") {
+                        alert('Please enter all value');
+                    } else {
+                        var item = new Item({
+                            title: $('.title-input').val(),
+                            url: $('.url-input').val(),
+                            price: $('.price-input').val(),
+                            priority: $('.priority-input').val(),
+                            user_id: $('.user-id').val()
+                        });
+                        $('.title-input').val('');
+                        $('.url-input').val('');
+                        $('.price-input').val('');
+                        $('.priority-input').val('');
+                        items.add(item);
+                        item.save({
+                            success: function (response) {
+                                _.each(response.toJSON(), function (item) {
+                                    console.log('Successfully Added an Item');
 //                                $('.container').load(location.href + ' .container');
-                            });
-                        },
-                        error: function () {
-                            console.log('Failed to get items!');
-                        }
-                    });
+                                });
+                            },
+                            error: function () {
+                                console.log('Failed to get items!');
+                            }
+                        });
+                    }
                 });
             }); //Add button
 
@@ -200,6 +213,12 @@
                     window.location.href = "<?php echo base_url(); ?>index.php/index_controller/index";
 <?php unset($_COOKIE['user_id']); ?>
 <?php unset($_COOKIE['username']); ?>
+                });
+            });
+
+            $(document).ready(function () {
+                $('.share-list').click(function (event) {
+                    window.location.href = "<?php echo base_url(); ?>index.php/index_controller/sharelist";
                 });
             });
 
