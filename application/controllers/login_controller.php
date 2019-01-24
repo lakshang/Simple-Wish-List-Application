91fa417e1d24061375e0889ee46567f49e976f6b
lakshang <lakshan.1998@gmail.com>
2019-01-24 10:00:22 +0530

    Add the login/register functions with working DB calls

--- application/controllers/login_controller.php
+++ application/controllers/login_controller.php
@@ -19,17 +19,37 @@ class login_controller extends REST_Controller {
     public function user_post() {
         $username = $this->post('username');
         $password = $this->post('password');
-
-        if (!$username || !$password) {
+        $name = $this->post('name');
+        $descrip = $this->post('descrip');
+        if (!$username || !$password || !$name || !$descrip) {
             $this->response("Info Error", 400);
         } else {
-            $result = $this->login_model->register(array("username" => $username, "password" => $password));
+            $result = $this->login_model->register(array("username" => $username, "password" => $password, "name" => $name, "description" => $descrip));
 
             if ($result === 0) {
                 $this->response("registration failed", 400);
             } else {
+                $result_id = $this->login_model->user_id($username);
+                $result_wishlist = $this->login_model->create_wishlist(array("user_id" => $result_id));
                 $this->response("registration successful", 200);
-                echo json_encode($result);
+                echo json_encode($result, $result_wishlist);
+            }
+        }
+    }
+
+    public function userLogin_post() {
+
+        $username = $this->post('username');
+        $password = $this->post('password');
+
+        if (!$username || !$password) {
+            $this->response("enter credentails", 400);
+        } else {
+            $result = $this->login_model->login($username, $password);
+            if ($result === FALSE) {
+                $this->response("invalid credentails, Try again.", 404);
+            } else {
+                $this->response("success", 200);
             }
         }
     }
