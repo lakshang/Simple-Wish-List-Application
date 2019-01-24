bc01dd35340657f625fdde68d17ac39bc757ce94
lakshang <lakshan.1998@gmail.com>
2019-01-24 10:00:50 +0530

    Modified the model to login/register functions to work

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -14,13 +14,15 @@
 class login_model extends CI_Model {
 
     private $table = "user";
+    private $table_list = "wishlist";
 
     public function __construct() {
         parent::__construct();
         $this->load->database();
+   
     }
 
-    function register($data) {
+    function register($data) { //username and password are params
         if ($this->db->insert($this->table, $data)) {
             return TRUE;
         } else {
@@ -35,6 +37,22 @@ class login_model extends CI_Model {
         $this->db->where('password', $password);
         $query = $this->db->get();
         if ($query->num_rows() == 1) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    function user_id($username) {
+        $this->db->select('user_id');
+        $this->db->from($this->table);
+        $this->db->where('username', $username);
+        $res = $this->db->get()->row()->user_id;
+        return $res;
+    }
+
+    function create_wishlist($data) {
+        if ($this->db->insert($this->table_list, $data)) {
             return TRUE;
         } else {
             return FALSE;
